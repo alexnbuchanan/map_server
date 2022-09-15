a48427f04ad29bfb8167bdc6151be6155dd9f24c
Alex Buchanan <alex.n.buchanan@gmail.com>
2022-09-14 19:13:39 -0700

    updated details

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors"); // to avoid CORS error: npm install cors
 const app = express();
 const { cloudinary } = require("./utils/cloudinary");
+
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -19,7 +21,7 @@ app.post("/api/upload", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3003;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
